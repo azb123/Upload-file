69bcb7ee07c5c17ca47ec9bc707d79e9d5900080
azb123 <azb123@126.com>
2021-08-21 09:14:56 +0800

    关卡11

---
+++ WWW/Pass-11/index.php
@@ -0,0 +1,66 @@
+<?php
+include '../config.php';
+include '../head.php';
+include '../menu.php';
+
+$is_upload = false;
+$msg = null;
+if(isset($_POST['submit'])){
+    $ext_arr = array('jpg','png','gif');
+    $file_ext = substr($_FILES['upload_file']['name'],strrpos($_FILES['upload_file']['name'],".")+1);
+    if(in_array($file_ext,$ext_arr)){
+        $temp_file = $_FILES['upload_file']['tmp_name'];
+        $img_path = $_GET['save_path']."/".rand(10, 99).date("YmdHis").".".$file_ext;
+        //因为php的底层全部用的c语言编写，而在c语言中，\0是结束符
+        // 在php中我们get方法使用%00进行阶段，而get是url传参
+        // 那么他进行url的编码，%0解码出来，就是我们\0
+        if(move_uploaded_file($temp_file,$img_path)){
+            $is_upload = true;
+        } else {
+            $msg = '上传出错！';
+        }
+    } else{
+        $msg = "只允许上传.jpg|.png|.gif类型文件！";
+    }
+}
+?>
+
+<div id="upload_panel">
+    <ol>
+        <li>
+            <h3>任务</h3>
+            <p>上传一个<code>webshell</code>到服务器。</p>
+        </li>
+        <li>
+            <h3>上传区</h3>
+            <form action="?save_path=../upload/" enctype="multipart/form-data" method="post">
+                <p>请选择要上传的图片：<p>
+                <input class="input_file" type="file" name="upload_file"/>
+                <input class="button" type="submit" name="submit" value="上传"/>
+            </form>
+            <div id="msg">
+                <?php 
+                    if($msg != null){
+                        echo "提示：".$msg;
+                    }
+                ?>
+            </div>
+            <div id="img">
+                <?php
+                    if($is_upload){
+                        echo '<img src="'.$img_path.'" width="250px" />';
+                    }
+                ?>
+            </div>
+        </li>
+        <?php 
+            if($_GET['action'] == "show_code"){
+                include 'show_code.php';
+            }
+        ?>
+    </ol>
+</div>
+
+<?php
+include '../footer.php';
+?>
